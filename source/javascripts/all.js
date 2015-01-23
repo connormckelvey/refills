8925b61237e0f8b11e3e856b174003ab6555cf97
Connor McKelvey <connormckelvey@gmail.com>
2015-01-23 14:13:43 -0500

    Initialize Fastclick

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -7,4 +7,5 @@
 $(document).ready(function() {
   $('#example').erToc({'goTopNode':'#example', 'startLevel': 'h2', 'numberedSuffix':'. '});
   $('.refills-menu-anchor').fixedsticky();
+  FastClick.attach(document.body);
 });
